78da98dc7f84e2790ad1817674819ddbe40ba078
Flavio Almeida <falmeida@c2r7s5.42lisboa.com>
2021-07-27 18:41:41 +0100

    Mini talk Mandatory Finished 27/07/2021

--- client.c
+++ client.c
@@ -6,74 +6,74 @@
 /*   By: falmeida <falmeida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 14:38:31 by falmeida          #+#    #+#             */
-/*   Updated: 2021/07/26 17:51:08 by falmeida         ###   ########.fr       */
+/*   Updated: 2021/07/27 18:38:50 by falmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_talk.h"
 
-void    send_message(int binario[], int pid)
+void	send_message(int binario[], int pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < 10)
-    {
-        if (binario[i] == 1)
-            kill(pid, SIGUSR1);
-        else if (binario[i] == 0)
-            kill(pid, SIGUSR2);
-        i++;
-        usleep(25);
-    }
+	i = 2;
+	while (i < 10)
+	{
+		if (binario[i] == 1)
+			kill(pid, SIGUSR1);
+		else if (binario[i] == 0)
+			kill(pid, SIGUSR2);
+		i++;
+		usleep(25);
+	}
 }
 
-void    pass_tob(int num, int pid)
+void	pass_tob(int num, int pid)
 {
-    int binario[10];
-    int i;
-    int j;
+	int	binario[10];
+	int	i;
+	int	j;
 
-    j = 0;
-    i = 10 - 1;
-    while (i > 0)
-    {
-        if (num % 2 == 0)
-            binario[i] = 0;
-        else
-            binario[i] = 1;
-        num = num / 2;
-        i--;
-    }
-    send_message(binario, pid);
+	j = 0;
+	i = 10 - 1;
+	while (i > 1)
+	{
+		if (num % 2 == 0)
+			binario[i] = 0;
+		else
+			binario[i] = 1;
+		num = num / 2;
+		i--;
+	}
+	send_message(binario, pid);
 }
 
-void    get_message(char *argv, int pid)
+void	get_message(char *argv, int pid)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (argv[i] != '\0')
-    {
-        pass_tob(argv[i], pid);
-        i++;
-    }
+	i = 0;
+	while (argv[i] != '\0')
+	{
+		pass_tob(argv[i], pid);
+		i++;
+	}
 }
 
-int get_pid(char **argv)
+int	get_pid(char **argv)
 {
-    int pid;
+	int	pid;
 
-    pid = ft_atoi(argv[1]);
-    return (pid);
+	pid = ft_atoi(argv[1]);
+	return (pid);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
+	int	pid;
 
-    if (argc == 1 || argc == 2 || argc > 3)
-        return (0);
-    pid = get_pid(argv);
-    get_message(argv[2], pid);
+	if (argc == 1 || argc == 2 || argc > 3)
+		return (0);
+	pid = get_pid(argv);
+	get_message(argv[2], pid);
 }
--- server.c
+++ server.c
@@ -6,59 +6,59 @@
 /*   By: falmeida <falmeida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/24 13:56:13 by falmeida          #+#    #+#             */
-/*   Updated: 2021/07/26 19:41:15 by falmeida         ###   ########.fr       */
+/*   Updated: 2021/07/27 18:31:16 by falmeida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_talk.h"
 
-void    ft_convert(int bin[])
+void	ft_convert(int bin[])
 {
-    int i;
-    int base;
-    int convert;
-    char c;
+	int		i;
+	int		base;
+	int		convert;
+	char	c;
 
-    base = 1;
-    i = 8;
-    convert = 0;
-    while (i > 0)
-    {
-        convert = convert + (base * bin[i]);
-        i--;
-        base = (base * 2);
-    }
-    c = convert;
-    write(1, &c, 1);
+	base = 1;
+	i = 7;
+	convert = 0;
+	while (i > 0)
+	{
+		convert = convert + (base * bin[i]);
+		i--;
+		base = (base * 2);
+	}
+	c = convert;
+	write(1, &c, 1);
 }
 
-void    getsignal(int i)
+void	getsignal(int i)
 {
-    int binario[9];
-    static int j;
+	int			binario[9];
+	static int	j;
 
-    if (i == SIGUSR1)
-        binario[j] = 1;
-    if (i == SIGUSR2)
-        binario[j] = 0;
-    j++;
-    if (j > 8)
-    {
-        ft_convert(binario);
-        j = 0;
-    }
+	if (i == SIGUSR1)
+		binario[j] = 1;
+	if (i == SIGUSR2)
+		binario[j] = 0;
+	j++;
+	if (j > 7)
+	{
+		ft_convert(binario);
+		j = 0;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    pid_t pid;
-    
-    int i;
-    i = 1;
-    pid = getpid();
-    signal(SIGUSR1, getsignal);
-    signal(SIGUSR2, getsignal);
-    printf("%d\n", pid);
-    while (1)
-        pause();
+	pid_t	pid;
+	int		i;
+
+	i = 1;
+	pid = getpid();
+	signal(SIGUSR1, getsignal);
+	signal(SIGUSR2, getsignal);
+	printf("%d\n", pid);
+	while (1)
+		pause();
 }
